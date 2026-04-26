2c0584576c3b3163ff73152caff4c05a32f42e1e
SorinGrajdian <sorin.grajdian@gmail.com>
2026-04-26 22:41:06 +0300

    Add project files.

---
+++ Rank/Models/Rating.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Rank.Models
+{
+    [Table("rank")]
+    public class RankModel
+    {
+        [Key]
+        public int ID { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public int Apreciere { get; set; }
+    }
+}
