c4d2ad2f075241ba22abaca29b816f0267423369
Petrache Iulian <ipetrache580@gmail.com>
2026-04-26 16:20:55 +0300

    Crearea schemei

---
+++ JS/logare.js
@@ -0,0 +1,20 @@
+const eroare = document.querySelector("#error");
+const input = document.querySelector("#login");
+const form = document.querySelector("#form");
+
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    const email = input.value;
+    validare(email);
+});
+
+function validare(email) {
+    const regex = /^[a-z]{2,20}\.[a-z]{2,20}@usm\.md$/;
+
+    if (regex.test(email)) {
+        window.location.replace("../HTML/pagina_idei.html");
+    } else {
+        eroare.style.display = "block";
+        input.value = "";
+    }
+}
